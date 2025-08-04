a7febb7bede88643230ff97856c88e9458a13b8c
MohamadMobin Rezaee <134555495+ALONE0007@users.noreply.github.com>
2025-08-04 18:43:58 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <test />
+</template>
+
+<script>
+import test from "./components/test.vue";
+
+export default {
+    name:'app',
+    components: {
+    test,
+  },
+}
+</script>
+
